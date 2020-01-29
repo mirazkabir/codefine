6013d7ea18913c947b88fe39660df605a9fa5707
mirazkabir <60362128+mirazkabir@users.noreply.github.com>
2020-01-29 13:18:35 +0600

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -4,27 +4,27 @@
  This is the link to your domain...it redirects the user to your myaccount page.
  It is used on the site on buttons like: Buy now, Order now, Members, Buy HTTP proxies, etc.
 */
-export const MEMBERS_URL = "https://members.url.goes.here";
+export const MEMBERS_URL = "https://myaccount.hmaproxy.com/";
 
 /*
  Your site name - this appears in the footer
 */
-export const SITE_NAME = "Site name goes here";
+export const SITE_NAME = "HMA Proxy:High Speed & Low Price Dedicated | Shared Proxies";
 
 /*
  The name of you site that appears in Terms of service section - it can be you domain, your company name, etc.
 */
-export const TOS_SITE_NAME = 'whatever-you-need';
+export const TOS_SITE_NAME = 'HMA Proxy';
 
 /*
  The date when the terms of service where last updated.
 */
-export const TOS_LAST_UPDATE = 'Apr-27-2018';
+export const TOS_LAST_UPDATE = 'Jan-27-2020';
 
 /*
  This is the link of your chat widget - it can be found on your tawk to account
 */
-export const TAWK_TO_WIDGET_URL = 'https://embed.tawk.to/5b61937de21878736ba285c5/default';
+export const TAWK_TO_WIDGET_URL = 'https://embed.tawk.to/5e312f6fdaaca76c6fd05d85/default';
 
 /*
  This is the link to your backend services. It is used to get the data for prices and datacenters. Normally, you shouldn't touch this one
@@ -34,6 +34,6 @@ export const BACKEND_URL = 'https://backend.changemyip.com';
 /*
  Meta tags for SEO - description and keywords for this site
 */
-export const SEO_DESCRIPTION = 'Fully anonymous private proxies (shared or dedicated)';
+export const SEO_DESCRIPTION = 'HMA Proxy provide Highly Anonymous and super fast dedicated/private proxies (shared or dedicated) for multiple countries of world at cheap price.';
 // The keywords need to be separated by comma
-export const SEO_KEYWORDS = 'proxy, socks, shared proxies, dedicated proxies, residential proxies, socksv5';
+export const SEO_KEYWORDS = 'hma proxy, fast proxy, cheap proxy, proxy, socks, shared proxies, dedicated proxies, residential proxies, socksv5';
